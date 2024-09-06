1d608970db2b280e8271899b87067df900ed105e
Matthias Volk <mv@infrablue.org>
2024-09-06 17:21:19 +0200

    FFT shader

--- src/waves.js
+++ src/waves.js
@@ -62,6 +62,61 @@ void main() {
 }
 `;
 
+const fftShader = `
+#define PI 3.1415926538
+
+uniform sampler2D u_input;
+
+uniform float u_size;       // 2**k
+uniform float u_subsize;    // 2**i
+uniform float u_horizontal;
+
+varying vec4 xy; // [-1, 1]
+
+float ratio;
+float arg_twiddle;
+
+float ix;      // [0, u_size]
+float ix_even; // [0, u_size / 2]
+float ix_odd;  // [u_size / 2, u_size]
+
+vec2 even;
+vec2 odd;
+vec2 twiddle;
+vec2 output;
+
+vec2 mul_complex(vec2 a, vec2 b) {
+    return vec2(a[0] * b[0] - a[1] * b[1], a[0] * b[1] + a[1] * b[0]);
+}
+
+void main() {
+
+    if (horizontal == 1) {
+        ix = (xy.x * 0.5 + 0.5) * u_size;
+    } else {
+        ix = (xy.y * 0.5 + 0.5) * u_size;
+    }
+
+    ratio = u_size / u_subsize; // 2**(k - i)
+    ix_even = mod(mod(ix, ratio / 2.0) + ratio * floor(ix / (ratio / 2.0)), u_size);
+    ix_odd = ix_even + ratio / 2.0;
+
+    arg_twiddle = -2.0 * PI * floor(ix / (ratio / 2.0)) / (2.0 * u_subsize);
+    twiddle = vec2(cos(arg_twiddle), sin(arg_twiddle));
+
+    if (horizontal == 1) {
+        even = texture2D(u_input, vec2(ix_even, gl_FragCoord.y) / u_size).xy;
+        odd = texture2D(u_input, vec2(ix_odd, gl_FragCoord.y) / u_size).xy;
+    } else {
+        even = texture2D(u_input, vec2(gl_FracCoord.x, ix_even) / u_size).xy;
+        odd = texture2D(u_input, vec2(gl_FracCoord.x, ix_odd) / u_size).xy;
+    }
+
+    output = even + mul_complex(twiddle, odd);
+    gl_FragColor = vec4(output, 0, 0);
+}
+`;
+
 const TWOPI = 2.0 * Math.PI;
 
 /**

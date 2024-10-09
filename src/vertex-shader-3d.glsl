ea8a3198c77ae57ba52eb75dda3491c9248a8b7e
Matthias Volk <mv@infrablue.org>
2024-10-09 19:10:57 +0200

    Cleanup

--- src/vertex-shader-3d.glsl
+++ src/vertex-shader-3d.glsl
@@ -9,18 +9,12 @@ uniform highp sampler2D u_displacements;
 
 uniform mat4 u_projection;
 uniform mat4 u_view;
-uniform vec2 u_modes;
 uniform vec2 u_scales;
 
 varying vec2 v_mappos;
 varying vec3 v_vertexpos;
 
-float phase;
 vec4 dis;
-
-vec2 mul_complex(vec2 a, vec2 b) {
-  return vec2(a[0] * b[0] - a[1] * b[1], a[0] * b[1] + a[1] * b[0]);
-}
 
 vec4 get_displacement(vec2 pos) {
   return texture2D(u_displacements, pos).zwxy;
